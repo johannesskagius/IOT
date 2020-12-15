09e43337634c8a8345c3029434591d93c2e28c3a
Johannes <johannes@johannesskagius@gmail.com>
2020-12-15 10:12:07 +0100

    Working app

--- app/src/main/java/com/example/node_red_01/Drone/Drone.java
+++ app/src/main/java/com/example/node_red_01/Drone/Drone.java
@@ -20,7 +20,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Drone {
-
+    private static final String POSITION_X = "position_X";
+    private static final String POSITION_Y = "position_Y";
+    private static final String NAME = "Name";
+    private static final String TYPE = "Service";
+    private final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private final DatabaseReference myRef = database.getReference();
+    private String []DONE_NAMES = {"drone1", "drone2"};
     private int ID;
     private String type;
     private int positionX;
@@ -34,6 +40,20 @@ public class Drone {
         this.positionY = positionY;
         this.refString = refString;
         dronePosition = new Position(positionX, positionY);
+        uploadDroneToServer();
+    }
+
+    private void uploadDroneToServer() {
+        DatabaseReference extended;
+        if (ID==1){
+        extended = myRef.child(DONE_NAMES[0]);
+        }else{
+            extended = myRef.child(DONE_NAMES[1]);
+        }
+        extended.child(NAME).setValue(ID);
+        extended.child(TYPE).setValue(TYPE);
+        extended.child(POSITION_X).setValue(positionX);
+        extended.child(POSITION_Y).setValue(positionY);
     }
 
     public Drone(int ID, String type) {
